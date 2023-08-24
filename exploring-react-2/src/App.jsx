3572f1ca0f0843db87baecc67bf7cbceb2200bc6
amaan8429 <mohdamaan8429@gmail.com>
2023-08-24 13:02:35 +0530

    initialised project 2

---
+++ exploring-react-2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+
+const App = () => {
+  <Router>
+    <Header />
+    <Routes>
+      <Route />
+    </Routes>
+  </Router>;
+};
+
+export default App;
